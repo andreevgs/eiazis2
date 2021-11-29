5dad15af08933a304abdad8295f0031ac290b980
Gleb <unicode256@yandex.by>
2021-11-29 20:12:44 +0300

    create referation

--- src/App.js
+++ src/App.js
@@ -3,81 +3,91 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [docContent, setDocContent] = useState('');
 
-  const [lang, setLang] = useState('');
+  const [result, setResult] = useState('');
   const [isFounded, setIsFounded] = useState(false);
+  const [file, setFile] = useState();
 
-  const handleCheckAlphabet = (e) => {
-    e.preventDefault();
-    const docObject = {
-      words: docContent,
-    };
-    axios.post('http://localhost:3000/analizator/words', docObject)
-        .then(response => {
-            if (!isFounded) {
-                setIsFounded(true);
-            }
-            setLang(response.data);
-            console.log(response);
-        })
-        .catch(error => {
-          alert('Не найдено, произошла ошибка');
-          console.log(error);
-        });
+    const uploadClickHandler = event => {
+        setFile(event.target.files[0])
+    }
 
+  const handleSentenceExtraction = (e) => {
+      e.preventDefault();
+      let formData = new FormData()
+      formData.append("file", file)
+
+      axios.post('http://localhost:3000/referat', formData, {
+          headers: {
+              'Content-Type': 'multipart/form-data'
+          }
+      })
+          .then(response => {
+              setResult(response.data)
+              setIsFounded(true)
+              console.log('result: ', response.data)
+          })
+          .catch(error => {
+              console.log('error: ', error)
+              setIsFounded(false);
+              alert('Произошла ошибка')
+          })
   }
 
-  const handleCheckWords = (e) => {
-    e.preventDefault();
-      const docObject = {
-          words: docContent,
-      };
-    axios.post(`http://localhost:3000/analizator/words`, docObject)
-        .then(response => {
-          if (!isFounded) {
-            setIsFounded(true);
+  const handleCheckNeural = (e) => {
+      e.preventDefault();
+      let formData = new FormData()
+      formData.append("file", file)
+
+      axios.post(`https://www.cs.cmu.edu/language/recognize&api_key=WNyyGDK5Ds4AP34hmjDb6FK1`, formData, {
+          headers: {
+              'Content-Type': 'multipart/form-data'
           }
-          setLang(response.data);
-          console.log(response);
-        })
-        .catch(error => {
-          setIsFounded(false);
-          alert('Не найдено, произошла ошибка');
-          console.log(error);
-        });
+      })
+          .then(response => {
+              setResult(response.data)
+              setIsFounded(true)
+              console.log('result: ', response.data)
+          })
+          .catch(error => {
+              console.log('error: ', error)
+              setIsFounded(false);
+              alert('Произошла ошибка')
+          })
   }
 
   return (
       <div className="App">
         <form method={'POST'}>
-            <h1>Распознавание языка текста несколькими способами</h1>
-          <h2>Введите текст</h2>
-          <textarea
-              name={'content'}
-              value={docContent}
-              placeholder={'содержимое'}
-              rows="10"
-              cols="45"
-              onChange={(e) => setDocContent(e.target.value)}
-          /><br />
+            <h1>Автоматическое реферирование документа</h1>
+          <h2>Прикрепите файл в формате txt и выберите методику реферирования</h2>
+          <input
+              type={'file'}
+              multiple
+              onChange={uploadClickHandler}
+          />
+            <br />
           <button
               type={'submit'}
-              onClick={handleCheckAlphabet}
+              onClick={handleSentenceExtraction}
           >
-            Проверить по алфавиту
+              Sentence Extraction
           </button>
-          <button
+            <button
               type={'submit'}
-              onClick={handleCheckWords}
+              onClick={handleCheckNeural}
+              style={{display: 'none'}}
           >
-            Проверить по словам
+            ML
           </button>
         </form>
         {isFounded && (
-            <div>
-              <h2>Язык текста: {lang}</h2>
-            </div>
+            <textarea
+                rows="10"
+                cols="45"
+            >
+                {result}
+            </textarea>
         )}
       </div>
   );
